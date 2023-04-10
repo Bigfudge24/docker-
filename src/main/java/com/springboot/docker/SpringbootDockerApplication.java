c4200dc6473675150c1dfa85c16c911cab1e9cf4
beugfudge <tomoyaa@tutamail.com>
2023-04-10 12:57:02 +0530

    initial commit

---
+++ src/main/java/com/springboot/docker/SpringbootDockerApplication.java
@@ -0,0 +1,22 @@
+package com.springboot.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDockerApplication {
+	
+	@GetMapping("/message")
+	public String getmessage() {
+		return "welcome to java";
+	}
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringbootDockerApplication.class, args);
+	}
+
+}
